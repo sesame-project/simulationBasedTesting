1d9da394c81079849e9c7c00ed0361dc48f0f1db
Thanos Zolotas <zolotas4@gmail.com>
2021-06-21 10:06:59 +0100

    Commands and auto start script.

---
+++ uk.ac.york.sesame.examples.ros.bridge/files/start.sh
@@ -0,0 +1,17 @@
+gnome-terminal --tab -- roscore
+wait 1
+echo "Done roscore"
+gnome-terminal --tab -- rosrun turtlesim turtlesim_node /turtle1/cmd_vel:=/turtle1/cmd_vel1
+wait 1
+echo "Done turtlesim"
+gnome-terminal --tab -- rosrun turtlesim turtle_teleop_key
+wait 1
+echo "Done turtle tele"
+gnome-terminal --tab -- roslaunch rosbridge_server rosbridge_websocket.launch
+wait 1
+echo "Done ros bridge"
+gnome-terminal --tab -- ~/Downloads/kafka_2.13-2.8.0/bin/zookeeper-server-start.sh ~/Downloads/kafka_2.13-2.8.0/config/zookeeper.properties
+wait 6
+gnome-terminal --tab -- ~/Downloads/kafka_2.13-2.8.0/bin/kafka-server-start.sh ~/Downloads/kafka_2.13-2.8.0/config/server.properties
+#bin/kafka-console-consumer.sh --topic .turtle1.cmd_vel --from-beginning --bootstrap-server localhost:9092
+
