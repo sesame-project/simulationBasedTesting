6752cd35d2447af4a337eb6c4b10aa5e2ff2c0af
Thanos Zolotas <zolotas4@gmail.com>
2021-08-06 11:37:39 +0100

    Added flink sink to OUT

--- uk.ac.york.sesame.testing.architecture.testing/src/uk/ac/york/sesame/testing/architecture/testing/TestingTestSuiteRunner.java
+++ uk.ac.york.sesame.testing.architecture.testing/src/uk/ac/york/sesame/testing/architecture/testing/TestingTestSuiteRunner.java
@@ -9,6 +9,7 @@ import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 
@@ -21,14 +22,15 @@ import uk.ac.york.sesame.testing.architecture.data.EventMessageSchema;
 public class TestingTestSuiteRunner {
 
 	public static void main(String[] args) throws Exception {
-	
+
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 		Properties properties = new Properties();
 		properties.setProperty("bootstrap.servers", "localhost:9092");
 		properties.setProperty("group.id", "test");
 		DataStream<EventMessage> stream = env
-			.addSource(new FlinkKafkaConsumer<EventMessage>("IN", new EventMessageSchema(), properties)).returns(EventMessage.class);
-		
+				.addSource(new FlinkKafkaConsumer<EventMessage>("IN", new EventMessageSchema(), properties))
+				.returns(EventMessage.class);
+
 		stream.map(new MapFunction<EventMessage, String>() {
 
 			@Override
@@ -37,7 +39,7 @@ public class TestingTestSuiteRunner {
 				return "";
 			}
 		});
-		
+
 		ROSSimulator rosSim = new ROSSimulator();
 		ConnectionProperties cp = new ConnectionProperties();
 		HashMap<String, Object> propsMap = new HashMap<String, Object>();
@@ -48,16 +50,16 @@ public class TestingTestSuiteRunner {
 
 		Thread simulator_runner_thread = new Thread() {
 			public void run() {
-				HashMap<String, String> params = new HashMap<String,String>();
-				params.put("launchPath", "/home/thanos/Documents/Git Projects/SESAME_WP6/uk.ac.york.sesame.examples.ros.bridge/files/start.sh");
+				HashMap<String, String> params = new HashMap<String, String>();
+				params.put("launchPath",
+						"/home/thanos/Documents/Git Projects/SESAME_WP6/uk.ac.york.sesame.examples.ros.bridge/files/start.sh");
 				System.out.println("Simulator Starts");
 				rosSim.run(params);
 			}
 		};
-		
+
 		simulator_runner_thread.start();
-		
-		
+
 //		Thread subscriber_thread__turtle1_pose = new Thread() {
 //			public void run() {
 //				System.out.println("Subscriber _turtle1_pose Starts");
@@ -65,7 +67,7 @@ public class TestingTestSuiteRunner {
 //			}
 //		};
 //		subscriber_thread__turtle1_pose.start();
-		
+
 		Thread subscriber_thread__turtle1_cmd_vel = new Thread() {
 			public void run() {
 				System.out.println("Subscriber _turtle1_cmd_vel Starts");
@@ -73,27 +75,33 @@ public class TestingTestSuiteRunner {
 			}
 		};
 		subscriber_thread__turtle1_cmd_vel.start();
-		
+
 		Thread from_out_to_sim = new Thread() {
 			public void run() {
 				System.out.println("From out to sim starts");
 				while (true) {
 //					ConsumerRecords<Long, EventMessage> cr = DataStreamManager.getInstance()
 //							.consume("/turtle1/cmd_vel1");
-					ConsumerRecords<Long, EventMessage> cr = DataStreamManager.getInstance()
-							.consume("OUT");
+					ConsumerRecords<Long, EventMessage> cr = DataStreamManager.getInstance().consume("OUT");
 					for (ConsumerRecord<Long, EventMessage> record : cr) {
 						System.out.println("Topic: " + record.value().getTopic().toString());
-						rosSim.publishToTopic(record.value().getTopic().toString().replace(".", "/"), record.value().getType(), record.value().getValue().toString());
+						rosSim.publishToTopic(record.value().getTopic().toString().replace(".", "/"),
+								record.value().getType(), record.value().getValue().toString());
 					}
 				}
 			}
 		};
 		from_out_to_sim.start();
+
+		// Kafka Sink to OUT
+		FlinkKafkaProducer<EventMessage> myProducer = new FlinkKafkaProducer<EventMessage>("OUT", // target topic
+				new EventMessageSchema(),
+				properties);
+
+		stream.addSink(myProducer);
+
 		env.execute();
 
-		
 	}
 
 }
-
--- uk.ac.york.sesame.testing.architecture/src/uk/ac/york/sesame/testing/architecture/data/EventMessageSchema.java
+++ uk.ac.york.sesame.testing.architecture/src/uk/ac/york/sesame/testing/architecture/data/EventMessageSchema.java
@@ -3,10 +3,12 @@ package uk.ac.york.sesame.testing.architecture.data;
 import java.io.IOException;
 
 import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.api.common.serialization.SerializationSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 
-public class EventMessageSchema implements DeserializationSchema<EventMessage> {
+public class EventMessageSchema implements DeserializationSchema<EventMessage>, SerializationSchema<EventMessage> {
 
 	static ObjectMapper objectMapper = new ObjectMapper();
 	@Override
@@ -24,6 +26,19 @@ public class EventMessageSchema implements DeserializationSchema<EventMessage> {
 	public boolean isEndOfStream(EventMessage nextElement) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public byte[] serialize(EventMessage element) {
+		if(objectMapper == null) {
+            objectMapper = new ObjectMapper();
+        }
+        try {
+            return objectMapper.writeValueAsString(element).getBytes();
+        } catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+        return new byte[0];
 	}
 
 }
