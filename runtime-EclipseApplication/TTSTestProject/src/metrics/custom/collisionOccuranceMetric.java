6add8804c8e6b99abcd2d8b574a7d36b116ea6e7
James Harbin <james.harbin@york.ac.uk>
2022-11-16 00:09:45 +0000

    First version of new metrics for the collision occurance for TTS

--- runtime-EclipseApplication/TTSTestProject/src/metrics/custom/collisionOccuranceMetric.java
+++ runtime-EclipseApplication/TTSTestProject/src/metrics/custom/collisionOccuranceMetric.java
@@ -1,48 +1,68 @@
 package metrics.custom;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 import org.apache.flink.api.common.state.*;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.streaming.api.functions.co.CoProcessFunction.Context;
 import org.apache.flink.util.Collector;
+import org.json.simple.*;
 
+import com.google.protobuf.ByteString;
+import com.google.protobuf.Message;
+import com.googlecode.protobuf.format.JsonFormat;
+
+import simlog.server.ROSMessage;
+import simlog.server.SafetyZone;
 import uk.ac.york.sesame.testing.architecture.data.EventMessage;
 import uk.ac.york.sesame.testing.architecture.metrics.Metric;
 
 public class collisionOccuranceMetric extends Metric {
 	
-
+	
+	
 	public collisionOccuranceMetric() {
+		
 	}
 
 	private static final long serialVersionUID = 1L;
 	private ValueState<Long> violationCount;
 	
-	private double distanceThreshold = 3.0;
+	private int levelThreshold = 30;
 	
 	// TODO: need some sort of timing grouping - only one output per interval
-	   
     public void open(Configuration parameters) throws Exception {
-
+    	violationCount = getRuntimeContext().getState(new ValueStateDescriptor<>("violationCount", Long.class));
     }
       
     public void processElement1(EventMessage msg, Context ctx, Collector<Double> out) throws Exception {
-    	// TODO: rewrite to find conditions for TTS stream
-    	String completionTopicName = "airframe_clearance";
+    	String completionTopicName = "safetyzone";
     	if (msg.getTopic().contains(completionTopicName)) {
-    		Object v = msg.getValue();
-    		Double dist = Double.valueOf((String)v);
     		
-    		if ((dist < distanceThreshold)) {
-    			System.out.println("VIOLATION: inner region " + msg);
-        		
-        		// Set initial value if not set
-        		if (violationCount.value() == null) {
-        			violationCount.update(0L);
-        		}
-        		
-        		// Increment the value
-        		violationCount.update(violationCount.value() + 1);
-        		out.collect(Double.valueOf(violationCount.value()));
+    		// TODO: if this works, factor out code from parsing ROSMessage!
+    		if (msg.getValue() instanceof String) {
+    			String s = (String) msg.getValue();
+    			JsonFormat jf = new JsonFormat();
+    			ROSMessage.Builder rosbuild = ROSMessage.newBuilder();
+    			InputStream stream = new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8));
+    			jf.merge(stream, rosbuild);
+    			ROSMessage rosmsg = rosbuild.build();
+    			
+    			SafetyZone sv = rosmsg.getSafetyZone();
+    			float level = sv.getLevel();
+    			String zoneID = sv.getZone();    		
+    			System.out.println("safetyzone message zone " + zoneID + " level " + level);
+      		
+    			if (violationCount == null) {
+    				violationCount.update(0L);
+    			}	
+      		
+    			if (level < levelThreshold) {
+    				violationCount.update(violationCount.value() + 1);
+    				System.out.println("violationCount output = " + violationCount);
+    				out.collect(Double.valueOf(levelThreshold));
+    			}
     		}
     	}
     }
