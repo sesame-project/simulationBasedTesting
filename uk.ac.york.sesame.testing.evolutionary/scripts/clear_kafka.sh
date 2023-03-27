80711f70d493a9294487563ee6927633cb8a0ebc
James Harbin <james.harbin@york.ac.uk>
2023-03-27 10:48:22 +0100

    Changes for clearing the Kakfa topic

--- uk.ac.york.sesame.testing.evolutionary/scripts/clear_kafka.sh
+++ uk.ac.york.sesame.testing.evolutionary/scripts/clear_kafka.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-cd ~/source/kafka/kafka_2.13-3.1.0/bin
-./kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic OUT &
-./kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic IN &
-./kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic metricMessages &
-./kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic controlMessages &
+KAFKA_SCRIPT_DIR=$HOME/source/kafka/kafka_2.13-3.1.0/bin
+
+$KAFKA_SCRIPT_DIR/kafka-delete-records.sh --bootstrap-server localhost:9092 --offset-json-file deletemsgs-IN.json
+$KAFKA_SCRIPT_DIR/kafka-delete-records.sh --bootstrap-server localhost:9092 --offset-json-file deletemsgs-OUT.json
+
+$KAFKA_SCRIPT_DIR/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic OUT &
+$KAFKA_SCRIPT_DIR/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic IN &
+$KAFKA_SCRIPT_DIR/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic metricMessages &
+$KAFKA_SCRIPT_DIR/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic controlMessages &
 echo "All cleared"
