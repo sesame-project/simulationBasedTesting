8d316f9c9320f0ef623a1ae985d9b3ac6c0001af
James Harbin <james.harbin@york.ac.uk>
2023-03-27 10:35:46 +0100

    Updates for the TTS/Kuka project for Windows
    (cherry picked from commit 9dba5af4f90d852263db383267d8fe1a5337e036)

---
+++ runtime-EclipseApplication/TTSTestProject/src/metrics/custom/jointValue.java
@@ -0,0 +1,25 @@
+package metrics.custom;
+
+import org.apache.flink.util.Collector;
+
+import uk.ac.york.sesame.testing.architecture.data.EventMessage;
+import uk.ac.york.sesame.testing.architecture.metrics.Metric;
+
+public abstract class jointValue extends Metric {
+
+	private static final long serialVersionUID = 1L;
+	
+	public jointValue() {
+		
+	}
+
+	protected abstract String getTopic();
+	
+	public void processElement1(EventMessage msg, Context ctx, Collector<Double> out) throws Exception {
+		String topic = getTopic();
+		if (msg.getTopic().contains(topic)) {
+			double val = Double.parseDouble((String)msg.getValue());
+			out.collect(val);
+		}
+	}
+}
