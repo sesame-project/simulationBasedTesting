fa08ac9096c96975a5e3d34b8867d5b8ce42d166
James Harbin <james.harbin@york.ac.uk>
2023-07-11 04:00:20 +0100

    Metric update

--- runtime-EclipseApplication/TTSTestProject/src/metrics/custom/collisionOccuranceMetric.java
+++ runtime-EclipseApplication/TTSTestProject/src/metrics/custom/collisionOccuranceMetric.java
@@ -1,49 +1,72 @@
 package metrics.custom;
 
+import java.util.Optional;
+
 import org.apache.flink.api.common.state.*;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.streaming.api.functions.co.CoProcessFunction.Context;
 import org.apache.flink.util.Collector;
 
+import simlog.server.ROSMessage;
+import simlog.server.SafetyZone;
 import uk.ac.york.sesame.testing.architecture.data.EventMessage;
-import uk.ac.york.sesame.testing.architecture.metrics.Metric;
+import uk.ac.york.sesame.testing.architecture.tts.ROSMessageConversion;
 
-public class collisionOccuranceMetric extends Metric {
-	
+// Using BatchedRateMetric for one per second interval
+public class collisionOccuranceMetric extends BatchedRateMetric {
+
+	private static final double TIME_BATCH_THRESHOLD = 1.0;
 
 	public collisionOccuranceMetric() {
+		super(TIME_BATCH_THRESHOLD);
 	}
 
 	private static final long serialVersionUID = 1L;
 	private ValueState<Long> violationCount;
-	
-	private double distanceThreshold = 3.0;
-	
-	// TODO: need some sort of timing grouping - only one output per interval
-	   
-    public void open(Configuration parameters) throws Exception {
 
-    }
-      
-    public void processElement1(EventMessage msg, Context ctx, Collector<Double> out) throws Exception {
-    	// TODO: rewrite to find conditions for TTS stream
-    	String completionTopicName = "airframe_clearance";
-    	if (msg.getTopic().contains(completionTopicName)) {
-    		Object v = msg.getValue();
-    		Double dist = Double.valueOf((String)v);
-    		
-    		if ((dist < distanceThreshold)) {
-    			System.out.println("VIOLATION: inner region " + msg);
-        		
-        		// Set initial value if not set
-        		if (violationCount.value() == null) {
-        			violationCount.update(0L);
-        		}
-        		
-        		// Increment the value
-        		violationCount.update(violationCount.value() + 1);
-        		out.collect(Double.valueOf(violationCount.value()));
-    		}
-    	}
-    }
+	private int getLevelThreshold() {
+		return 300;
+	}
+
+	public void open(Configuration parameters) throws Exception {
+		super.open(parameters, "collisionOccuranceMetric");
+		violationCount = getRuntimeContext().getState(new ValueStateDescriptor<>("violationCount", Long.class));
+	}
+	
+	private boolean topicMatches(String topic) {
+		//return topic.contains("Z1") || topic.contains("Z2") || topic.contains("Z3");
+		return true;
+	}
+
+	public void processElement1(EventMessage msg, Context ctx, Collector<Double> out) throws Exception {
+		
+		String completionTopicName = "safetyzone";
+		String topic = msg.getTopic();
+		if (topic.contains(completionTopicName) && topicMatches(topic)) {
+
+			System.out.println("ZZZ SAFETYZONE processElement1 - " + topic);
+			if (msg.getValue() instanceof String) {
+				String s = (String) msg.getValue();
+				Optional<ROSMessage> rosmsg_o = ROSMessageConversion.fromJsonString(s);
+				if (rosmsg_o.isPresent()) {
+					ROSMessage rosmsg = rosmsg_o.get();
+					SafetyZone sv = rosmsg.getSafetyZone();
+					float level = sv.getLevel();
+					String object1 = sv.getObject1();
+					String object2 = sv.getObject2();
+					String zoneID = sv.getZone();
+					System.out.println("safetyzone message zone " + zoneID + ",object1 = " + object1 + ",object2=" + object2 + " level " + level);
+
+					if (violationCount.value() == null) {
+						violationCount.update(0L);
+					}
+
+					//if (level < getLevelThreshold() && isReadyToLogNow()) {
+						violationCount.update(violationCount.value() + 1);
+						System.out.println("violationCount output = " + violationCount.value());
+						out.collect(Double.valueOf(violationCount.value()));
+					//}
+				}
+			}
+		}
+	}
 }
