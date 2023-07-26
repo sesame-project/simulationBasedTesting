5f823a0f31ae472c1470c78f4ae275b00ef3cb02
James Harbin <james.harbin@york.ac.uk>
2023-07-27 00:28:33 +0100

    New script

---
+++ run_standalone_generator.sh
@@ -0,0 +1,1 @@
+./run_standalone_generator_params.sh /home/jharbin/academic/sesame/WP6/ /home/jharbin/academic/sesame/WP6/uk.ac.york.sesame.testing.standalone.generator/models/testingKUKA_TTS.model /tmp/codegen/
