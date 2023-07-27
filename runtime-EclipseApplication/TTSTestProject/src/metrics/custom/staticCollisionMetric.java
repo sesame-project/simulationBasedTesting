6d3fb1355ba38bd7ccc87350fe9c20824cdfa7e1
James Harbin <james.harbin@york.ac.uk>
2023-07-27 15:07:10 +0100

    New metrics

--- runtime-EclipseApplication/TTSTestProject/src/metrics/custom/staticCollisionMetric.java
+++ runtime-EclipseApplication/TTSTestProject/src/metrics/custom/staticCollisionMetric.java
@@ -4,7 +4,7 @@ public class staticCollisionMetric extends combinedCollisionMetric {
 	private static final long serialVersionUID = 1L;
 
 	protected boolean topicMatches(String topic) {
-		return false;
+		return topic.contains("Z1") || topic.contains("Z2") || topic.contains("Z3"); 
 	}
 
 	protected boolean isObjectMatching(String object1, String object2) {
