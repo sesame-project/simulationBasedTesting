424bac4d9187669d4df3a1a63b52e6898271d0d7
James Harbin <james.harbin@york.ac.uk>
2023-11-11 16:35:08 +0000

    New joint value metric for protocol testing

---
+++ runtime-EclipseApplication/TTSTestProject/src/metrics/custom/joint3ValueMetric.java
@@ -0,0 +1,10 @@
+package metrics.custom;
+
+public class joint3ValueMetric extends jointValue {
+
+	private static final long serialVersionUID = 1L;
+
+	protected String getTopic() {
+		return "Kuka.j3";
+	}
+}
