3a3ea8899a1d516e47d6b4f058658495c11fd18a
James Harbin <james.harbin@york.ac.uk>
2023-12-06 14:01:12 +0000

    New metric definition and A50 model

--- runtime-EclipseApplication/TTSTestProject/src/metrics/custom/collisionDriveShaftMetric.java
+++ runtime-EclipseApplication/TTSTestProject/src/metrics/custom/collisionDriveShaftMetric.java
@@ -1,48 +1,53 @@
 package metrics.custom;
 
+import java.util.HashMap;
+
 import org.apache.flink.api.common.state.*;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.util.Collector;
 
 import uk.ac.york.sesame.testing.architecture.data.EventMessage;
+import uk.ac.york.sesame.testing.architecture.tts.customtypes.SafetyZone;
 //import uk.ac.york.sesame.testing.architecture.tts.customtypes.SafetyZone;
 
 // Using BatchedRateMetric for one per second interval
 public class collisionDriveShaftMetric extends BatchedRateMetric {
 
 	private static final double TIME_BATCH_THRESHOLD = 1.0;
+	private static final double LEVEL_THRESHOLD_FOR_COLLISION = 2.0;
 
 	public collisionDriveShaftMetric() {
 		super(TIME_BATCH_THRESHOLD);
 	}
 
 	private static final long serialVersionUID = 1L;
-	private ValueState<Long> violationCount;
+	private ValueState<Double> violationMax;
 
 	public void open(Configuration parameters) throws Exception {
 		super.open(parameters, "collisionOccuranceMetric");
-		violationCount = getRuntimeContext().getState(new ValueStateDescriptor<>("violationCount", Long.class));
+		violationMax = getRuntimeContext().getState(new ValueStateDescriptor<>("violationMax", Double.class));
 	}
 	
 	public void processElement1(EventMessage msg, Context ctx, Collector<Double> out) throws Exception {
-		
 		String completionTopicName = "SafetyZone";
 		String topic = msg.getTopic();
 		if (topic.contains(completionTopicName)) {
-			if (msg.getValue() instanceof String) {
-				String s = (String) msg.getValue();
-				if (s.contains("GSZ")) {
-					System.out.println("Safetyzone message in collsionDriveShaft=" + s);
-					
-					if (violationCount.value() == null) {
-						violationCount.update(0L);
-					}
+			HashMap<String,Object> safetyZone = (HashMap<String, Object>) msg.getValue();
+			String zone = (String)safetyZone.get("zoneID");
+			Double level = (Double)safetyZone.get("level");
 
-					if (isReadyToLogNow()) {
-						violationCount.update(violationCount.value() + 1);
-						System.out.println("violationCount output = " + violationCount);
-						out.collect(Double.valueOf(violationCount.value()));
-					}
+			// Ensure the max is set to zero at the start
+			if (violationMax.value() == null) {
+				violationMax.update(0.0);
+			}
+			
+			if (zone.contains("GSZ") && (level > LEVEL_THRESHOLD_FOR_COLLISION)) {
+				System.out.println("Safetyzone message in collsionDriveShaft=" + zone + "-" + level);
+					
+				if (violationMax.value() < level) {
+					violationMax.update(level);
+					System.out.println("violationMax increased to " + level);
+					out.collect(level);
 				}
 			}
 		}
