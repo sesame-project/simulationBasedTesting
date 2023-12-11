dba1d551de757092535ab3d61674cced64a68fde
James Harbin <james.harbin@york.ac.uk>
2023-12-11 03:13:11 +0000

    Fixes for metric M1

--- runtime-EclipseApplication/PALTesting/src/metrics/custom/M1_countObjectsDeliveredMetric.java
+++ runtime-EclipseApplication/PALTesting/src/metrics/custom/M1_countObjectsDeliveredMetric.java
@@ -1,9 +1,8 @@
 package metrics.custom;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
 
@@ -26,42 +25,44 @@ import uk.ac.york.sesame.testing.architecture.utilities.ParsingUtils;
 // Then when crossing a target point and loaded, increment the delivered count
 // Only send at limited rate, e.g. max once per 0.1 seconds
 
-public class M1_countObjectsDelivered extends BatchedRateMetric {
+public class M1_countObjectsDeliveredMetric extends BatchedRateMetric {
 
 	private static final long serialVersionUID = 1L;
 	
 	// Gives the count of object on a particular robot
 	protected MapState<String,Integer> objectCountForRobot;
 	protected MapState<String,Boolean> loadedStatus;
-	protected MapState<Point3D,Set<Integer>> waypointDeliveryState;
+	
+	// Key is a String of the point - to ensure it is the same
+	// Value is a comma-separated string of object IDs O1,O2 etc rather than a set
+	protected MapState<String, String> waypointDeliveryState;
 	
 	protected ValueState<Integer> deliveredCount;
 	protected ValueState<Double> sendTime;
 	
-	protected final ArrayList<Point3D> targetPoints;
-    
-    private static double SEND_RATE_LIMIT_TIME = 0.1;
+	private ValueState<Boolean> pmb2LoadedState;
+	private ValueState<Boolean> omniLoadedState;
+	 
+    private static double SEND_RATE_LIMIT_TIME = 5.0;
 	
 	private class LoadedChangeTracker extends statusChangeTracker {
 
 		private String robotName;
-		private LoadedChangeTracker(String robotName) {
+		private LoadedChangeTracker(String robotName, ValueState<Boolean> lastStatus) {
+			super(lastStatus);
 			this.robotName = robotName;
 		}
 		
 		protected String getRobotString() {
-			// TODO Auto-generated method stub
-			return null;
+			return robotName;
 		}
 
 		protected String getContentString() {
-			return null;
-			// "/conserts/pal/loaded"
+			return "true";
 		}
 
 		protected String getCompletionTopicName() {
-			// TODO Auto-generated method stub
-			return null;
+			return "/conserts/pal/loaded";
 		}
 
 		protected void statusChangeAction(boolean currentStatus) {
@@ -78,39 +79,59 @@ public class M1_countObjectsDelivered extends BatchedRateMetric {
 		}
 	}
 	
-	LoadedChangeTracker pmbLoadedTracker = new LoadedChangeTracker("pmb_2");
-	LoadedChangeTracker omniLoadedTracker = new LoadedChangeTracker("omni_base");
+	transient LoadedChangeTracker pmbLoadedTracker;
+	transient LoadedChangeTracker omniLoadedTracker;
 
-	public M1_countObjectsDelivered() {
+	public M1_countObjectsDeliveredMetric() {
 		super(SEND_RATE_LIMIT_TIME);
-		targetPoints = new ArrayList<Point3D>();
+		//targetPoints = new ArrayList<Point3D>();
 		// Add a waypoint - need both 1 and 2
-		targetPoints.add(new Point3D(-7.382,3.24,0.0));
+		//
 	}
 	
 	public void incrementObjectCount(String robotName) throws Exception {
 		if (!objectCountForRobot.contains(robotName)) {
 			objectCountForRobot.put(robotName, 1);
-			metricLog("Incremented object count to 1");
+			metricLog("Incremented object count for " + robotName + " to 1");
 		} else {
 			Integer currentCount = objectCountForRobot.get(robotName);
 			objectCountForRobot.put(robotName, currentCount + 1);
-			metricLog("Incremented object count to " + objectCountForRobot.get(robotName));
+			metricLog("Incremented object count for " + robotName + "to " + objectCountForRobot.get(robotName));
 		}
 	}
 	
 	private void setLoadedStatus(String robotName, boolean status) {
 		try {
+			metricLog("Set loaded status for " + robotName + " to " + status);
 			loadedStatus.put(robotName, status);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	private boolean getLoadedStatus(String robotName) throws Exception {
+		Boolean s = loadedStatus.get(robotName);
+		if (s == null) {
+			return false;
+		} else {
+			return s;
+		}
+	}
 
 	public void open(Configuration parameters) throws Exception {
 		sendTime = getRuntimeContext().getState(new ValueStateDescriptor<>("M1-sentTime", Double.class));
+		objectCountForRobot = getRuntimeContext().getMapState(new MapStateDescriptor<>("M1-objectCountTracker", String.class, Integer.class));
+		pmb2LoadedState = getRuntimeContext().getState(new ValueStateDescriptor<>("M1-pmb2loadedStatus", Boolean.class));
+		omniLoadedState = getRuntimeContext().getState(new ValueStateDescriptor<>("M1-omniloadedStatus", Boolean.class));
+		loadedStatus = getRuntimeContext().getMapState(new MapStateDescriptor<>("M1-loadedStatusMap", String.class, Boolean.class));
+		waypointDeliveryState = getRuntimeContext().getMapState(new MapStateDescriptor<>("M1-waypointdeliverystate", String.class, String.class));
 		objectCountForRobot = getRuntimeContext().getMapState(new MapStateDescriptor<>("objectCountTracker", String.class, Integer.class));
-		super.open(parameters);
+		
+		pmbLoadedTracker = new LoadedChangeTracker("pmb2_1", pmb2LoadedState);
+		omniLoadedTracker = new LoadedChangeTracker("omni_base_1", omniLoadedState);
+		
+		super.open(parameters, "M1objectCount");
+		metricLog("Setup done");
 	}
 
 	protected boolean positionTopicMatches(EventMessage msg) {
@@ -134,6 +155,9 @@ public class M1_countObjectsDelivered extends BatchedRateMetric {
        
     public void processElement1(EventMessage msg, Context ctx, Collector<Double> out) throws Exception {
     	
+    	pmbLoadedTracker.checkMessage(msg);
+    	omniLoadedTracker.checkMessage(msg);
+    	
     	if (positionTopicMatches(msg)) {
     		Object value = msg.getValue();
 			Object obj = JSONValue.parse(value.toString());
@@ -148,19 +172,23 @@ public class M1_countObjectsDelivered extends BatchedRateMetric {
     		
 				Point3D current = new Point3D(x,y,z);
 				// List of target points to scan
-				for (Point3D targetPoint : targetPoints) {
+				for (Point3D targetPoint : getTargetPoints()) {
 					double dist = current.distanceToOther(targetPoint);
 					
 					// Check distance
-					// TODO: Also need to check the robot VELOCITY here to see if it's stopped
+					// TODO: Also need to check the robot VELOCITY here to see if it's stopped!
     				if (dist < getDistThreshold()) {
 						double timeNow = SimCore.getInstance().getTime();
-						registerObjectDelivery(robotName, targetPoint, timeNow, dist, out);
+						checkObjectDelivery(robotName, targetPoint, timeNow, dist, out);
 					}
 				}
     		}
     	}
     }
+
+	private Point3D[] getTargetPoints() {
+		return new Point3D[] { new Point3D(-3.359, 3.207, 0 ), new Point3D(-5.35,3.39,0.0) };
+	}
 
 	private Optional<String> getRobotName(String topic) {
 		if (topic.contains("pmb2_1")) {
@@ -174,18 +202,33 @@ public class M1_countObjectsDelivered extends BatchedRateMetric {
 		return Optional.empty();
 	}
 
-	private void registerObjectDelivery(String robotName, Point3D targetPoint, double timeNow, double dist, Collector<Double> out) {
+	private void checkObjectDelivery(String robotName, Point3D targetPoint3D, double timeNow, double dist, Collector<Double> out) {
 		// Register the delivery of object n to waypoint
 		try {
-			int objectIDForRobot = objectCountForRobot.get(robotName);
-			Set<Integer> objs = waypointDeliveryState.get(targetPoint);
-			if (objs.contains(objectIDForRobot)) {
-				objs.add(objectIDForRobot);
+			boolean robotIsLoaded = getLoadedStatus(robotName);
+			String targetPoint = targetPoint3D.toString();
+					
+			if (objectCountForRobot.contains(robotName) && robotIsLoaded) {
+				metricLog("registerObjectDelivery - loaded robot looking for " + robotName);
+				int objectIDForRobot = objectCountForRobot.get(robotName);
+				String objString = "O" + String.valueOf(objectIDForRobot);
+
+				String objs;
+				if (!waypointDeliveryState.contains(targetPoint)) {
+					objs = ""; 
+				} else {
+					objs = waypointDeliveryState.get(targetPoint);
+				}
+								
+				if (!objs.contains(objString)) {
+					objs = objs + "," + objString;
+				}
+				
+				waypointDeliveryState.put(targetPoint, objs);
+				// Ensure we only send the total on the first item - if trouble, send of every entry
+				// to the zone
+				sendObjectTotal(out);
 			}
-			// Ensure we only send the total on the first item - if trouble, send of every entry
-			// to the zone
-			sendObjectTotal(out);
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -193,10 +236,26 @@ public class M1_countObjectsDelivered extends BatchedRateMetric {
 
 	private void sendObjectTotal(Collector<Double> out) throws Exception {
 		int totalForWaypoints = 0;
-		for (Set<Integer> me : waypointDeliveryState.values()) {
-			totalForWaypoints += me.size();
+		for (Entry<String, String> me : waypointDeliveryState.entries()) {
+			String objString = me.getValue();
+			int size = countObjects(objString);
+			totalForWaypoints += size;
+			metricLog("Waypoint " + me.getKey() + " has " + objString + ": count as " + size + " objects");
 		}
 		
-		out.collect((double)totalForWaypoints);
+		metricLog("Sending out object total " + totalForWaypoints);
+		if (isReadyNow()) {
+			out.collect((double)totalForWaypoints);
+		}
+	}
+
+	private int countObjects(String objString) {
+		int Ototal = 0;
+		for (int i = 0; i < objString.length(); i++) {
+			if (objString.charAt(i) == 'O') {
+				Ototal++;
+			}
+		}
+		return Ototal;
 	}
 }
