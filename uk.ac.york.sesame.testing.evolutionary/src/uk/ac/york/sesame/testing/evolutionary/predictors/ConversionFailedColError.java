a2253e3798a38aef3ee072cadc52bfbf9db37c53
James Harbin <james.harbin@york.ac.uk>
2024-11-26 02:55:18 +0000

    Predictor testing new exception files

---
+++ uk.ac.york.sesame.testing.evolutionary/src/uk/ac/york/sesame/testing/evolutionary/predictors/ConversionFailedColError.java
@@ -0,0 +1,9 @@
+package uk.ac.york.sesame.testing.evolutionary.predictors;
+
+public class ConversionFailedColError extends Exception {
+
+	public ConversionFailedColError(Double orig) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ uk.ac.york.sesame.testing.evolutionary/src/uk/ac/york/sesame/testing/evolutionary/predictors/MissingColumnFor.java
@@ -0,0 +1,11 @@
+package uk.ac.york.sesame.testing.evolutionary.predictors;
+
+import uk.ac.york.sesame.testing.dsl.generated.TestingPackage.FuzzingOperations.FuzzingOperation;
+
+public class MissingColumnFor extends Exception {
+
+	public MissingColumnFor(FuzzingOperation op) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
